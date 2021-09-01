f75b3e923c8060d7bb5aa60d48bb5738063989d2
mamunarr6 <mamunarr6@gmail.com>
2021-09-02 00:37:10 +0600

    html designing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log('hello');
+const searchField = document.getElementById('search-field');
+const searchButton = document.getElementById('search-button');
+const bookContainer = document.getElementById('book-container');
+
+searchButton.addEventListener('click', () => {
+    const searchText = searchField.value;
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayBook(data.docs));
+
+});
+
+const displayBook = books => {
+    books.forEach(book => {
+        console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+                <div class="rounded">
+                    <img style="width: 150px; height: 200px; border-radius: 2px;"
+                        src="https://covers.openlibrary.org/b/id/${c}-M.jpg" alt="">
+                </div>
+                <div class="my-3">
+                    <h2 class="text-secondary fw-1">${book.title}</h2>
+                    <p class="text-secondary">${book.author_alternative_name}r</p>
+                    // <p class="text-secondary">author</p>
+                    <p class="text-secondary">${book.publisher}</p>
+                    <p class="text-secondary">${book.first_publish_year}</p>
+                </div>
+        `;
+    })
+}
